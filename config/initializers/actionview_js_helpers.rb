a2997dd0778b9ecbd1958c82a26435ad3876b3af
Alejandro Juarez <alex@fisica.unam.mx>
2010-06-23 18:25:43 -0500

    Adding jquery and jquery-ui

---
+++ config/initializers/actionview_js_helpers.rb
@@ -0,0 +1,2 @@
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery-1.4.2', 'rails', 'jquery.ui/jquery-ui-1.8.1.custom']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jrails => ['jrails.min']
