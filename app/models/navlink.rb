9fff47eb6e4c5885a544c427557d772210f57f2f
Alejandro Juarez <alex@fisica.unam.mx>
2010-06-25 17:47:43 -0500

    Adding title_with_ancestors, is_parent? and section_children methods in navlink model

--- app/models/navlink.rb
+++ app/models/navlink.rb
@@ -7,8 +7,33 @@ class Navlink < ActiveRecord::Base
 
   belongs_to :navlinkable, :polymorphic => true
 
-  def is_parent?
-    (children.size > 0 or parent.nil? or navlinkable.nil?) and url.to_s.empty?
+  def self.all_section_links
+    all_main_links.order('title ASC').all.collect { |record|
+      if record.is_parent?
+        self.section_children(record)
+      else
+        record
+      end
+    }.flatten.compact
   end
 
+  def self.section_children(parent)
+      section = [parent]
+      parent.children.each do |record|
+          if record.is_parent?
+            section.push(section_children(record))
+          else
+            section.push(record)
+          end
+      end
+      section.flatten.compact
+  end
+
+  def title_with_ancestors
+    self_and_ancestors.collect {|a| a.title }.join(' - ')
+  end
+
+  def is_parent?
+      (children.size > 0 or parent.nil? or navlinkable.nil?) and url.to_s.empty?
+  end
 end
