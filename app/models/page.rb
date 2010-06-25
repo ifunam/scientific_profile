1ee4eac8bbc08a63705faa433254a69f7771e196
Alejandro Juarez <alex@fisica.unam.mx>
2010-06-25 12:12:42 -0500

    Adding image model and carrierwave uploader integration

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,7 @@
 class Page < ActiveRecord::Base
   validates_presence_of :title, :body
+  
   belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
   has_one :navlink, :as => :navlinkable, :dependent => :destroy
+  has_one :image, :as => :imageable, :dependent => :destroy
 end
