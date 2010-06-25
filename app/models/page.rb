e3d7de6586fbb11f6e6db9f310b2ad1f6df45ad0
Alejandro Juarez <alex@fisica.unam.mx>
2010-06-25 17:49:22 -0500

    Adding default scope, callback based on associate_to_navlink_section method and minor changes

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,21 @@
 class Page < ActiveRecord::Base
+  attr_accessor :navlink_section_id
+
   validates_presence_of :title, :body
-  
+
   belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
-  has_one :navlink, :as => :navlinkable, :dependent => :destroy
-  has_one :image, :as => :imageable, :dependent => :destroy
+  has_one :navlink_section, :as => :navlinkable, :dependent => :destroy
+  has_one :image, :as => :imageable
+  accepts_nested_attributes_for :image
+
+  default_scope :order => 'title ASC, updated_at DESC'
+
+  after_save :associate_to_navlink_section
+
+  def associate_to_navlink_section
+    if :new_record? and navlink_section_id.to_i > 0
+      navlink = Navlink.create!(:navlinkable => self, :title => self.title)
+      navlink.move_to_child_of self.navlink_section_id.to_i
+    end
+  end
 end
