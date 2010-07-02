8398f68e09246d8227b8503f0017b2e0b0a9694c
Alejandro Juarez <alex@fisica.unam.mx>
2010-07-02 13:04:17 -0500

    Adding basic named_scope for article and article_status models

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,12 +5,22 @@ class Article < ActiveRecord::Base
   validates_numericality_of :id, :journal_id, :only_integer => true, :greater_than => 0, :allow_nil => true
   validates_numericality_of :article_status_id, :only_integer => true, :greater_than => 0
   validates_numericality_of :year, :greater_than => (Date.today.year - 100), :less_than_or_equal_to => (Date.today.year + 1), :only_integer => true
+  validates_numericality_of :month, :greater_than => 0, :less_than_or_equal_to => 12, :only_integer => true, :allow_nil => true
+
   validates_uniqueness_of :title, :scope => [:journal_id, :year]
 
   belongs_to :journal
   belongs_to :article_status
+
   has_one :document, :as => :documentable, :dependent => :destroy
   accepts_nested_attributes_for :document
+
+  default_scope :order => 'year DESC, month DESC, title ASC, authors ASC, article_status_id ASC'
+
+  scope :accepted, where(:article_status_id => 1)
+  scope :sent, where(:article_status_id => 2)
+  scope :inprogress, where(:article_status_id => 3)
+  scope :published, where(:article_status_id => 4)
 
   def as_text
     [authors, title, journal.name, journal_issue, pages].compact.join(', ')
--- app/models/article_status.rb
+++ app/models/article_status.rb
@@ -1,4 +1,6 @@
 class ArticleStatus < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   has_many :articles
+  
+  default_scope :order => 'name ASC'
 end
