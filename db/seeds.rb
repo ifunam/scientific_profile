14fe48c9e2b1d779e34d598adda6cfcc09832f01
Alejandro Juarez <alex@fisica.unam.mx>
2010-07-02 12:33:54 -0500

    Updating db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,16 @@
   Navlink.create!(:title => title)
 end
 
-['Investigador', 'Investigador posdoctoral', 'Estudiante'].each do |name|
+['Investigador', 'Colaboradores', 'Estudiante'].each do |name|
   Group.create(:name => name)
 end
 
 User.create(:email => 'admin@heaven.com', :password => 'soydios', :password_confirmation => 'soydios', :is_admin => true)
+
+['Aceptado', 'Enviado', 'En preparación', 'Publicado'].each do |name|
+  ArticleStatus.create(:name => name)
+end
+
+['Applied Physics B', 'Classical and Quantum Gravity', 'Physical Review A'].each do |name|
+  Journal.create(:name => name)
+end
