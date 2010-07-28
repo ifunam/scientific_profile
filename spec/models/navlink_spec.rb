ee293b5513fa47600c9cbf6cea3c13585da4c540
Alejandro Juárez <alexjr85@gmail.com>
2010-07-28 13:21:51 -0500

    Adding Gemfile.lock into dotgitignore file

--- spec/models/navlink_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Navlink do
-  it { should validate_presence_of(:title) }
-  it { should belong_to(:parent) }
-  it { should validate_numericality_of(:parent_id, :allow_blank => true) }
-
-end
--- spec/models/page_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Page do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
-  it { should belong_to(:author) }
-  it { should have_one(:navlink) }
-
-end
