63b254543e80641d2e7e948ae5301aa9fe56f91c
Alejandro Juárez <alexjr85@gmail.com>
2010-07-29 09:52:29 -0500

    Removing unnecessary comments

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,45 +1,32 @@
 class BooksController < ApplicationController
   respond_to :html
 
-  # GET /pages
-  # GET /pages.xml
   def index
     respond_with(@books = BookEdition.all.paginate(:page => params[:page] || 1, :per_page => params[:per_page] || 10))
   end
 
-  # GET /pages/1
-  # GET /pages/1.xml
   def show
     respond_with(@book = Book.find(params[:id]))
   end
 
-  # GET /pages/new
-  # GET /pages/new.xml
   def new
     respond_with(@book = Book.new)
   end
 
-  # GET /pages/1/edit
   def edit
     respond_with(@book = Book.find(params[:id]))
   end
 
-  # POST /pages
-  # POST /pages.xml
   def create
     respond_with(@book = Book.create(params[:book]), :status => :created)
   end
 
-  # PUT /pages/1
-  # PUT /pages/1.xml
   def update
     @book = Book.find(params[:id])
     @book.update_attributes(params[:book])
     respond_with(@book, :status => :updated)
   end
 
-  # DELETE /pages/1
-  # DELETE /pages/1.xml
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
