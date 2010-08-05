46dbec9d0fa26bc11e91991f5a57bf3f8683db72
Alejandro Juarez <alex@fisica.unam.mx>
2010-08-05 10:24:53 -0500

    Adding scope based on scope_by_fuzzy

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -3,4 +3,6 @@ class Journal < ActiveRecord::Base
  validates :name, :presence => true, :uniqueness => true
  has_many :articles
  default_scope :order => 'name ASC'
+
+ scope_by_soundex :search, :fields => [:name]
 end
